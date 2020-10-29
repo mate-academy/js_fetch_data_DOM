75022299c3e78054636ef7b538082b5a682b1475
AntonChetveryk <chetverykanton92@gmail.com>
2020-10-29 20:41:33 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
+/* eslint-disable no-console */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-undef */
+/* eslint-disable operator-linebreak */
 'use strict';
 
-// write your code here
+const BASE_URL =
+  'https://mate-academy.github.io/phone-catalogue-static/api/phones';
+
+const request = (url) => {
+  return new Promise((resolve) => {
+    fetch(url)
+      .then((res) => {
+        if (res.status < 400) {
+          return res.json();
+        } else {
+          throw res;
+        }
+      })
+      .then((res) => resolve(res));
+  });
+};
+
+const getPhones = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Timeout exceeded')), 5000);
+    request(`${BASE_URL}.json`).then((res) => resolve(res));
+  });
+};
+
+const getPhonesIDs = async () => {
+  const phones = await getPhones();
+
+  return phones.map((phone) => phone.id);
+};
+
+const IDs = getPhonesIDs();
+
+const getPhonesDetails = async (arr) => {
+  const res = await arr.map((id) => {
+    const phoneDetail = request(`${BASE_URL}/${id}.json`);
+
+    return phoneDetail;
+  });
+
+  return Promise.all(res);
+};
+
+IDs.then((arrOfIDs) => getPhonesDetails(arrOfIDs)).then((res) =>
+  console.log(res)
+);
