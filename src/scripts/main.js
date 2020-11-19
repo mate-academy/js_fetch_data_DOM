749e20d68d42bf036e1973b919c58c674411964e
dmitry <akulinind@gmail.com>
2020-11-19 18:41:27 +0300

    add request and getPhones methods

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+/* eslint-disable no-console */
+const BASE_URL
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const request = (url) => {
+  // eslint-disable-next-line no-undef
+  return fetch(`${BASE_URL}${url}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = () => {
+  return new Promise((resolve, reject) => {
+    resolve(request('/phones.json'));
+
+    setTimeout(() => {
+      reject(new Error('Timeout'));
+    }, 5000);
+  });
+};
+
+getPhones()
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
