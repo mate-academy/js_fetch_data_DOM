2b2fb4a47c31d48eda5e613a4c4877923d66b601
Andrii Hrushetskyi <andrii.hrushetskii@gmail.com>
2021-01-17 02:53:14 -0500

    should be solutions
    but there can be bugs with node modules as well

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,45 @@
+/* eslint-disable max-len */
 'use strict';
 
-// write your code here
+const getPhones = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject(new Error('Phones didnt get, timed out'));
+    }, 5000);
+
+    fetch('https://mate-academy.github.io/phone-catalogue-static/api/phones.json')
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch('something went wrong');
+  });
+};
+
+const getPhonesDetails = phonesIds => {
+  return new Promise((resolve, reject) => {
+    const phonesDetails = [];
+
+    phonesIds.forEach((phoneId, i, array) => {
+      fetch(`https://mate-academy.github.io/phone-catalogue-static/api/phones/${phoneId}.json`)
+        .then(response => response.json())
+        .then(data => {
+          phonesDetails.push(data);
+
+          if (array.length === phonesDetails.length) {
+            resolve(phonesDetails);
+          }
+        })
+        .catch((error) => reject(new Error(`ERROR: ${error}`)));
+    });
+  });
+};
+
+const phonesArrivingListener = getPhones();
+
+phonesArrivingListener
+  .then(data => getPhonesDetails(data.map(phone => phone.id)))
+  .then(details => details.forEach(detail => {
+    document.querySelector('#list')
+      .insertAdjacentHTML('beforeend', `<li>${JSON.stringify(detail)}</li>`);
+  }))
+  .catch(error => document.querySelector('#list')
+    .insertAdjacentHTML('beforeend', `<li>${error}</li>`));
