5f9431a9afb9e982003bef290a65691816fb1198
Bohdan Dobak <chudolipton@gmail.com>
2021-03-12 20:47:52 +0200

    made fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+const listUrl
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const detailsUrl
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+
+function getPhones(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(result => resolve(result));
+
+    setTimeout(() => reject(new Error('end')), 5000);
+  });
+}
+
+function getPhonesDetails(idsData) {
+  const items = idsData.map(id => fetch(`${detailsUrl}${id}.json`));
+
+  return Promise.all(items);
+}
+
+const phonesIdsData = [];
+
+const list = document.createElement('ul');
+
+getPhones(listUrl)
+  .then(result => {
+    for (const item of result) {
+      const listItem = document.createElement('li');
+
+      listItem.textContent = item.name;
+      list.append(listItem);
+      phonesIdsData.push(item.id);
+    }
+
+    return phonesIdsData;
+  })
+  .then(items => getPhonesDetails(items));
+document.body.append(list);
