c5a490d2dfa3d4233b8ad46fbd948e2f005872cf
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-04-02 19:52:52 +0300

    small changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,35 +3,32 @@
 const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
 const options = {
   method: 'GET',
-};
 
+  // headers: {
+  //   'content-type': 'application/json; charset=utf-8',
+  // },
+};
 const body = document.querySelector('body');
 
 const getPhones = (endPoint) => {
-  return new Promise((resolve, reject) => {
-    fetch(`${BASE_URL}${endPoint}`, options)
-      .then(response => {
-        if (!response.ok) {
-          return Promise.reject(new Error(
-            `${response.status} - ${response.statusText}`
-          ));
-        }
+  return fetch(`${BASE_URL}${endPoint}`, options)
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(new Error(
+          `${response.status} - ${response.statusText}`
+        ));
+      }
 
-        return resolve(response.json());
-      });
-
-    setTimeout(() => {
-      reject(new Error());
-    }, 5000);
-  });
+      return response.json();
+    });
 };
 
-const getPhonesDetails = (listOfPhones) => {
-  const newList = listOfPhones.map(phone => phone.id).map(id => {
+const getPhonesDetails = (list) => {
+  const newList = list.map(phone => phone.id).map(id => {
     fetch(`${BASE_URL}/phones/${id}.json`);
   });
 
-  return Promise.all(newList);
+  return newList;
 };
 
 const parentElemnt = document.createElement('ul');
