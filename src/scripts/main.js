02ab99779aa8617d863ef1d9f091151b73504cb8
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-04-02 19:34:31 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write your code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+const options = {
+  method: 'GET',
+};
+
+const body = document.querySelector('body');
+
+const getPhones = (endPoint) => {
+  return new Promise((resolve, reject) => {
+    fetch(`${BASE_URL}${endPoint}`, options)
+      .then(response => {
+        if (!response.ok) {
+          return Promise.reject(new Error(
+            `${response.status} - ${response.statusText}`
+          ));
+        }
+
+        resolve(response.json());
+
+        setTimeout(() => {
+          reject(new Error());
+        }, 5000);
+      });
+  });
+};
+
+const getPhonesDetails = (listOfPhones) => {
+  const newList = listOfPhones.map(phone => phone.id).map(id => {
+    fetch(`${BASE_URL}/phones${id}.json`);
+  });
+
+  return Promise.all(newList);
+};
+
+const parentElemnt = document.createElement('ul');
+
+body.append(parentElemnt);
+
+getPhones('/phones.json')
+  .then(result => {
+    for (let i = 0; i < result.length; i++) {
+      const liElement = document.createElement('li');
+
+      parentElemnt.append(liElement);
+      liElement.append(result[i].name);
+    }
+  })
+  .then(getPhonesDetails)
+  .catch(error => {
+    alert(error);
+  });
