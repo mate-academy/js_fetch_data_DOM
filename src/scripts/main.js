6c7db8ddef99e1cf9428715124096f48dd618be6
Bohdan Hnatuk <hnatuk996@gmail.com>
2021-08-16 21:19:43 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,38 @@
 'use strict';
 
 // write your code here
-const url = 'https://mate-academy.github.io/'
-  + 'phone-catalogue-static/api/phones.json';
+const url = 'https://mate-academy.github.io/phone-catalogue-static/api/';
 
 const ul = document.createElement('ul');
 
 const getPhones = () => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-
-        return Promise.reject(
-          new Error(response.status)
-        );
-      })
-      .catch(error => (ul.innerHTML = error))
-      .then(mobiles => {
-        ul.innerHTML = (mobiles.map(x => (
-          `<li>${x.name}</li>`
-        )).join(''));
-
-        setTimeout(() => reject(new Error()), 5000);
-      });
+  return new Promise((resolve) => {
+    resolve(fetch(url + 'phones.json')
+      .then(request => request.json())
+    );
+  }).catch(message => {
+    setTimeout(() => {
+      ul.innerHTML = `${message}`;
+    }, 5000);
   });
 };
 
-getPhones();
+const getPhonesDetails = (ids) => {
+  const result = Promise.all(ids.map(id => {
+    return new Promise((resolve) => {
+      return resolve(url + `phones.${id}.json`);
+    });
+  }));
+
+  return result;
+};
+
+getPhones().then(mobiles => {
+  mobiles.map(mobile => ul.insertAdjacentHTML('afterbegin', `
+    <li>${mobile.name}</li>
+    `));
+});
+
+getPhones().then(response => getPhonesDetails(response.map(phone => phone.id)));
+
 document.body.append(ul);
