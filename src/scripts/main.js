9548ff559387e5ecf8f1bafe6565150a81b903b2
Bohdan Hnatuk <hnatuk996@gmail.com>
2021-08-18 18:06:22 +0300

    fixed 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,22 +6,19 @@ const url = 'https://mate-academy.github.io/phone-catalogue-static/api/';
 const ul = document.createElement('ul');
 
 const getPhones = () => {
-  return new Promise((resolve) => {
-    resolve(fetch(url + 'phones.json')
-      .then(request => request.json())
-    );
-  }).catch(message => {
-    setTimeout(() => {
-      ul.innerHTML = `${message}`;
-    }, 5000);
-  });
+  return fetch(url + 'phones.json')
+    .then(request => request.json())
+    .catch(message => {
+      setTimeout(() => {
+        ul.innerHTML = `${message}`;
+      }, 5000);
+    });
 };
 
 const getPhonesDetails = (ids) => {
   const result = Promise.all(ids.map(id => {
-    return new Promise((resolve) => {
-      return resolve(url + `phones.${id}.json`);
-    });
+    return fetch(url + `phones/${id}.json`)
+      .then(request => request.json());
   }));
 
   return result;
