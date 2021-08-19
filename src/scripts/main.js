6fd37c89fe338c12525d9480998e235247377c29
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-19 22:18:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
 'use strict';
 
-// write your code here
+const listURL
+= `https://mate-academy.github.io/phone-catalogue-static/api/phones.json`;
+
+const detailsURL
+= `https://mate-academy.github.io/phone-catalogue-static/api/phones/`;
+
+const newList = document.createElement('ul');
+
+document.body.insertAdjacentElement('beforeend', newList);
+
+function getPhones() {
+  return fetch(listURL)
+    .then(response => response.json());
+}
+
+function getPhonesDetails(id) {
+  return fetch(`${detailsURL}` + `${id}.json`)
+    .then(response => response.json());
+}
+
+getPhones().then(result => {
+  const phonesIds = result.map(phone => phone.id);
+
+  for (const key of phonesIds) {
+    getPhonesDetails(key).then(phone => {
+      newList.insertAdjacentHTML('afterbegin', `
+      <li>
+      PHONE NAME: ${phone.name}
+      <br>
+      PHONE DETAILS: ${phone.id}
+      </li>
+      `);
+    });
+  };
+});
