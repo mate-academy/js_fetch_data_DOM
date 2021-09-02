d513d0effddf39b75ac8f5b7b640541dc0af9c5b
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-02 13:31:45 +0300

    fetch-data-DOM-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write your code here
+const phonesUrl
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+const detailUrl
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+const phonesList = document.createElement('ul');
+
+const getPhones = () => {
+  return fetch(phonesUrl)
+    .then((response) => response.json())
+    .catch(error => {
+      setTimeout(() => {
+        Error(error);
+      }, 5000);
+    });
+};
+
+const getPhonesDetails = (id) => {
+  return fetch(`${detailUrl}` + `${id}.json`)
+    .then((response) => response.json())
+    .catch((error) => new Error(error));
+};
+
+getPhones()
+  .then(list => {
+    const listPhoneDetail = list.map(phone => getPhonesDetails(`${phone.id}`));
+
+    Promise.all([...listPhoneDetail])
+      .then(phoneDetails => {
+        phoneDetails.forEach(item => {
+          const phone = document.createElement('li');
+
+          phone.textContent = item.id;
+          phonesList.append(phone);
+        });
+      });
+  });
+
+document.body.append(phonesList);
