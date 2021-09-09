4a4deb424367e61ea459fb5e778985065594ffc4
Andriy Fesych <andriyhimself@gmail.com>
2021-09-09 22:57:06 +0300

    Solution_0.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const url = 'https://mate-academy.github.io/phone-catalogue-static/api/phones';
+
+const list = document.createElement('ul');
+
+document.body.append(list);
+
+function makeLi(text) {
+  const li = document.createElement('li');
+
+  li.innerText = text;
+
+  list.append(li);
+}
+
+function getPhones() {
+  return fetch(url + '.json')
+    .then(info => info.json())
+    .then(data => {
+      return data.map(element => element.id);
+    })
+    .catch(error => {
+      return setTimeout(() => {
+        alert('Error:', error);
+      }, 5000);
+    });
+}
+
+function getPhonesDetails(idsList) {
+  const arrayOfPromises = idsList.map(someId => {
+    return fetch(url + `/${someId}.json`)
+      .then(somePhone => somePhone.json())
+      .catch(error => alert('Error:', error));
+  });
+
+  return Promise.all(arrayOfPromises);
+}
+
+getPhones()
+  .then(data => getPhonesDetails(data))
+  // this promise returns combined data of all phones
+  // how can I save it to access it from the upper scope?
+  .then(phonesWithDetails => {
+    phonesWithDetails.forEach(phone => {
+      makeLi(phone.name);
+    });
+  });
