6001a7f4d8d21f5af91dc1cb04dd989117e71712
MaxStets <weawer458@gmail.com>
2021-10-13 20:40:35 +0300

    Solution -- fetch_data_DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+const list = document.createElement('ul');
+
+document.body.append(list);
+
+const request = (
+  endPoint = '',
+  dataFormat = '.json',
+  params = { method: 'GET' },
+  baseUrl = 'https://mate-academy.github.io/phone-catalogue-static/api'
+) => {
+  return fetch(`${baseUrl}${endPoint}${dataFormat}`, params)
+    .then(response => {
+      if (!response.ok) {
+        setTimeout(() => {
+          return new Error('Error with request');
+        });
+      };
+
+      return response.json();
+    });
+};
+
+const getPhones = () => {
+  return request('/phones');
+};
+
+const getPhonesDetails = (id) => {
+  return request(`/phones/${id}`);
+};
+
+getPhones()
+  .then(data => {
+    return Promise.all(data.map(el => getPhonesDetails(el.id)));
+  })
+  .then(phones => {
+    phones.forEach(phone => {
+      list.insertAdjacentHTML(
+        'beforeend',
+        `<li>${phone.name}</li>`
+      );
+    });
+  });
