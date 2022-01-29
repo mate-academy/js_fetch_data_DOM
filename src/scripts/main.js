44051b6c7e6bbd455c2fe708bd619e7af1641119
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2022-01-29 23:56:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write your code here
+const baseUrl
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones';
+
+const getPhones = () => {
+  return new Promise((resolve, reject) => {
+    fetch(`${baseUrl}.json`)
+      .then(response => response.json())
+      .then(phones => {
+        resolve(phones);
+      });
+
+    setTimeout(() => reject(new Error()), 5000);
+  });
+};
+
+const showNames = (names) => {
+  const list = document.createElement('ul');
+
+  for (const phone of names) {
+    const item = document.createElement('li');
+
+    item.textContent = phone;
+    list.append(item);
+  }
+
+  document.body.append(list);
+};
+
+function getPhonesDetails(IDs) {
+  const phonesDetails = [];
+
+  return new Promise((resolve, reject) => {
+    for (const id of IDs) {
+      fetch(`${baseUrl}/${id}.json`)
+        .then(response => response.json())
+        .then(details => {
+          phonesDetails.push(details);
+        })
+        .catch(() => reject(new Error('Something wrong')));
+    }
+
+    resolve(phonesDetails);
+  });
+};
+
+getPhones()
+  .then((phones) => {
+    const allIDs = phones.map(phone => phone.id);
+
+    const allNames = phones.map(phone => phone.name);
+
+    showNames(allNames);
+    getPhonesDetails(allIDs);
+  });
