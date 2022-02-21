d164902765ecf122247daa27b87c91082f02f5d5
Roman Kovalchuk <roma220702@gmail.com>
2022-02-21 21:07:33 +0300

    some message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,61 @@
 'use strict';
 
-// write your code here
+const BASE_URL = `https://mate-academy.github.io
+/phone-catalogue-static/api/phones.json`;
+const URLdetails = `https://mate-academy.github.
+io/phone-catalogue-static/api/phones/`;
+
+const request = () => {
+  return fetch(`${BASE_URL}`)
+    .then(response => {
+      if (!response.ok) {
+        setTimeout(() => {
+          throw Error(`${response.status} - ${response.statusText}`);
+        }, 5000);
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = () => request();
+
+function getPhonesDetails(arrayOfIds) {
+  const arr = arrayOfIds.map(phoneId => {
+    return fetch(`${URLdetails}/${phoneId}.json`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(`${response.status} - ${response.statusText}`);
+        }
+
+        return response.json();
+      });
+  });
+
+  return arr;
+}
+
+getPhones()
+  .then(result => {
+    const table = document.createElement('table');
+    let i = 1;
+    const arrPhonesId = result.map(el => {
+      table.insertAdjacentHTML('beforeend', `
+        <tr>
+          <td style="border: 1px solid blue;">${i++}</td>
+          <td style="border: 1px solid blue;">${el.name}</td>
+        </tr>
+      `);
+
+      return el.id;
+    });
+
+    document.body.append(table);
+
+    getPhonesDetails(arrPhonesId)
+      .then(resolve => {
+        return resolve;
+      });
+  })
+
+  .catch();
