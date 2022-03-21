99a52cc547b8d69851c5ac7ac7e1e7100c73fba7
Misha Novikov <novikov1807@gmail.com>
2022-03-21 17:55:02 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write your code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/';
+const bodyTag = document.querySelector('body');
+
+document.querySelector('h1').style.display = 'none';
+
+bodyTag.insertAdjacentHTML('afterbegin',
+  `<table>
+    <tbody id="listOfPhones"></tbody>
+  </table>`);
+
+const table = document.getElementById('listOfPhones');
+const request = (url = '') => {
+  return fetch(`${BASE_URL}api/phones${url}.json`)
+    .then((response) => {
+      setTimeout(() => {
+        if (!response.ok) {
+          return Promise.reject(
+            new Error(`${response.status} - not found`)
+          );
+        }
+      }, 1000);
+
+      return response.json();
+    });
+};
+
+const getPhones = () => request();
+
+const getPhonesDetails = (phoneUrl) => request(`/${phoneUrl}`);
+
+getPhones()
+  .then(data => {
+    data.map(phone => {
+      getPhonesDetails(phone.id)
+        .then(phoneDetails => {
+          table.insertAdjacentHTML('beforeend',
+            `<tr>
+              <td>${phoneDetails.id})</td>
+              <td>${phoneDetails.android.os}</td>
+              <td><img src=${BASE_URL}${phoneDetails.images[0]}></td>
+            </tr>
+          `);
+        });
+    });
+  });
