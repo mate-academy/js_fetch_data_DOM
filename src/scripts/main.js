b99b8ba37b093c1196c33ccefde7ea57e42dd3da
Vladyslav317 <you@example.com>
2022-05-17 23:10:33 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,50 @@
 'use strict';
 
-// write your code here
+const BaseUrl
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+const phoneDetails
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+
+const body = document.body;
+const ul = document.createElement('ul');
+
+body.append(ul);
+
+function getPhones() {
+  return fetch(BaseUrl)
+    .then(response => {
+      if (!response.ok) {
+        return setTimeout(() => {
+          throw Error(`${response.status} - ${response.statusText}`);
+        }, 5000);
+      }
+
+      return response.json();
+    });
+}
+
+function getPhonesDetails(phoneIDs) {
+  phoneIDs.forEach(phoneid => {
+    fetch(`${phoneDetails}${phoneid}.json`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(`${response.status} - ${response.status.text}`);
+        }
+
+        return response.json();
+      })
+      .then(phone => {
+        const li = document.createElement('li');
+
+        li.textContent = phone.name;
+        ul.append(li);
+      });
+  });
+};
+
+getPhones()
+  .then(phones => {
+    const phoneIDs = phones.map(phone => phone.id);
+
+    getPhonesDetails(phoneIDs);
+  });
