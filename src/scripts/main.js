3bb5f0a311748c9ec646f86fc5217502d9f6af49
user <user@Igors-MacBook-Air.local>
2022-05-20 16:45:39 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
-// write your code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const request = (url) => {
+  return fetch(`${BASE_URL}${url}`)
+    .then((response) => response.json());
+};
+
+const phonesList = document.createElement('ul');
+
+document.body.append(phonesList);
+
+const getPhones = () => {
+  request('/phones.json')
+    .then(phones => {
+      const phonesIdList = phones.map(item => item.id);
+
+      getPhonesDetails(phonesIdList);
+    })
+    .catch(error => {
+      setTimeout(() => {
+        Error(error);
+      }, 5000);
+    });
+};
+
+const getPhonesDetails = (phonesIdList) => {
+  phonesIdList.map(id => {
+    request(`/phones/${id}.json`)
+      .then(item => {
+        const li = document.createElement('li');
+
+        li.innerText = item.name;
+        phonesList.append(li);
+      })
+      .catch(error => {
+        Error(error);
+      });
+  });
+};
+
+getPhones();
