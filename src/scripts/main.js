b5da71e6434105190e7672569e16eef9270bc765
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-06-17 15:58:20 +0300

    I am tormented by vague doubts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const urlList
+ = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const ul = document.createElement('ul');
+
+document.body.append(ul);
+
+const request = (url) => {
+  return fetch(url)
+    .then(response => {
+      return response.ok
+        ? response.json()
+        : setTimeout(() => {
+          throw new Error('Something wrong');
+        }, 5000);
+    });
+};
+
+const getPhones = () => request(`${urlList}.json`)
+  .then(result => {
+    const idxs = result.map(phone => phone.id);
+
+    getPhonesDetails(idxs);
+  });
+
+const getPhonesDetails = (arrayIds) => {
+  arrayIds.forEach(id => request(`${urlList}/${id}.json`)
+    .then(data => ul.insertAdjacentHTML('beforeend', `
+      <li>Name: ${data.name}
+        <ul>
+          <li>${data.display.screenResolution}</li>
+          <li>${data.android.os}</li>
+          <li>${data.storage.ram}</li>
+        </ul>
+      </li>
+    `))
+  );
+};
+
+getPhones();
