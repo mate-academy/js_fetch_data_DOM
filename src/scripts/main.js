b39a07f580fd4887a51e53ba88bbc23e4770a56c
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-07-14 15:08:23 +0200

    make build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const url = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const request = () => {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        return Promise.reject(Error('Somethin went erong'));
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = () => {
+  return request();
+};
+
+const getPhonesDetails = () => {
+  return request()
+    .then(item => {
+      const arrOfNames = [];
+
+      item.map(el => arrOfNames.push(el.name));
+
+      const div = document.createElement('div');
+      const ul = document.createElement('ul');
+
+      div.appendChild(ul);
+
+      arrOfNames.forEach(el => ul.insertAdjacentHTML('afterbegin',
+        `<li>${el}</li>`,
+      ));
+
+      document.body.appendChild(div);
+    });
+};
+
+function printV(value) {
+  console.log(value);
+}
+
+getPhones()
+  .then(printV);
+
+getPhonesDetails()
+  .then(printV);
