917e796ffc0bbf9a7562100624a23bcef4344a32
KostyaKonev <kosgoh.214@gmail.com>
2022-07-15 21:35:13 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const LIST = `
+  https://mate-academy.github.io/phone-catalogue-static/api/phones.json
+`;
+const DETAILS = `
+  https://mate-academy.github.io/phone-catalogue-static/api/phones/:phoneId.json
+`;
+
+const ul = document.createElement('ul');
+
+document.body.append(ul);
+
+const getPhones = () => {
+  return fetch(LIST)
+    .then(res => {
+      setTimeout(() => {
+        if (!res.ok) {
+          throw new Error('phones not found');
+        }
+      }, 5000);
+
+      return res.json();
+    });
+};
+
+const getPhonesDetails = (phones) => {
+  phones.forEach(phone => {
+    fetch(DETAILS.replace(':phoneId', phone.id))
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('phones not found');
+        }
+
+        return res.json();
+      })
+      .then(preparedProne => {
+        const li = document.createElement('li');
+
+        li.innerText = preparedProne.name;
+        li.classList.add('active');
+
+        ul.append(li);
+      });
+  });
+};
+
+getPhones().then(phones => getPhonesDetails(phones));
