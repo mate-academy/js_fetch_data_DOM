affc004272339564fdb2f50ec844a4abff6db213
Olexandr Istomin <gn.53@ukr.net>
2022-07-21 15:56:28 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
 'use strict';
 
-// write your code here
+const ul = document.createElement('ul');
+
+function request(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok
+      && !response.headers.get('content-type').includes('application/json')) {
+        return Promise.reject(new Error(`${response.status}`));
+      }
+
+      return response.json();
+    });
+}
+
+function getPhones(url) {
+  return request(url);
+}
+
+function getPhonesDetails(array) {
+  const phonesInfo = [];
+
+  for (const i of array) {
+    request(`https://mate-academy.github.io/`
+    + `phone-catalogue-static/api/phones/${i.id}.json`)
+      .then(response => {
+        const li = document.createElement('li');
+
+        li.innerText = response.name;
+        ul.append(li);
+        phonesInfo.push(i);
+      })
+      .catch((error) => {
+        return new Error(error);
+      });
+  }
+
+  document.body.append(ul);
+
+  return phonesInfo;
+}
+
+getPhones('https://mate-academy.github.io/'
++ 'phone-catalogue-static/api/phones.json')
+  .then(response => {
+    getPhonesDetails(response);
+  })
+  .catch(error => {
+    setTimeout(() => {
+      document.body.append(error);
+    }, 5000);
+  });
