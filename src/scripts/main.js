f10b0ad5c044f1898910838e1b5731893c6fec74
Alyona Pisotska <pisotska.o@gmail.com>
2022-08-03 17:38:50 +0300

    Fetch data DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const list = document.createElement('ul');
+const BASE_URL
+  = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const request = () => {
+  return fetch(`${BASE_URL}`)
+    .then(response => {
+      if (!response.ok) {
+        setTimeout(() => {
+          return Promise.reject(
+            new Error(`${response.status} - ${response.statusText}`)
+          );
+        }, 5000);
+      }
+
+      return response.json();
+    });
+};
+
+const getPhones = () => request();
+
+const getPhonesDetails = () => {
+  request()
+    .then(result => {
+      result.forEach(item => {
+        const listItem = document.createElement('li');
+
+        listItem.innerText = item.name;
+        list.append(listItem);
+      });
+    });
+
+  body.append(list);
+};
+
+getPhones();
+getPhonesDetails();
