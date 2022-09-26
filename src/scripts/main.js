e25554c5b61257431705aeb2832a4fb303db8f44
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-09-27 05:06:07 +0530

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,59 @@
-'use strict';
+('use strict');
 
-// write your code here
+const controller = new AbortController();
+
+setTimeout(() => {
+  controller.abort();
+  throw new Error('request timed out');
+}, 5000);
+
+const phonesDetails = [];
+
+const Baseurl = 'https://mate-academy.github.io/phone-catalogue-static/api';
+
+const request = (url) => {
+  return fetch(`${Baseurl}${url}`, {
+    signal: controller.signal,
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
+};
+
+function displayData(data) {
+  for (const item of data) {
+    const phonesUl = document.querySelector('#phones-list');
+    const phonesLi = document.createElement('li');
+    const p = document.createElement('p');
+
+    phonesLi.className = 'phone-item';
+    p.className = 'mb-0';
+
+    p.innerHTML = item.name;
+
+    phonesLi.append(p);
+    phonesUl.append(phonesLi);
+  }
+}
+
+const getPhones = () => {
+  return request('/phones.json').then((data) => {
+    displayData(data);
+  });
+};
+
+const getPhonesDetails = (phoneIds) => {
+  for (const phone of phoneIds) {
+    request(`/phones/${phone}.json`)
+      .then((data) => {
+        phonesDetails.push(data);
+      })
+      .catch((error) => new Error(error));
+  }
+};
+
+getPhones();
+getPhonesDetails();
