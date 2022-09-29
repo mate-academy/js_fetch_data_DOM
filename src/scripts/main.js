031d0274c21d65ac38e7e85e4fd20e6c752d28ac
fatihsamur <a.fatihsamur@gmail.com>
2022-09-29 08:14:57 +0300

    fetched

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+/* eslint-disable */
+
+const ListURL = 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const detailsURL = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/:phoneId.json';
+
+/* eslint-enable */
+
+renderPhones();
+
+function renderPhones() {
+  const phoneListDOM = document.querySelector('.phone_list');
+
+  getPhones(ListURL)
+    .then(phones => {
+      const phoneNames = phones.map(phone => phone.name);
+
+      phoneNames.forEach(phoneName => {
+        const li = document.createElement('li');
+
+        li.innerText = phoneName;
+        phoneListDOM.appendChild(li);
+      });
+    });
+}
+
+function request(url) {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw Error();
+      }
+
+      return response.json();
+    });
+}
+
+function getPhones(url) {
+  return request(url);
+}
+
+/* function getPhoneDetails (url) {
+  return request(url);
+} */
+
+// getPhoneDetails(detailsBaseURL + 'motorola-xoom-with-wi-fi' + '.json');
