ce20eef5729d5f095a68328cd53e2a32a616474e
bokorzita <104830913+bokorzita@users.noreply.github.com>
2022-10-28 16:52:19 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
-// write your code here
+const url
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+/* const getPhones
+  = fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    }); */
+
+const controller = new AbortController();
+const timeoutId = setTimeout(() => controller.abort(), 5000);
+
+const getPhones
+  = fetch(url, { signal: controller.signal })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+
+clearTimeout(timeoutId);
+
+let html = '';
+
+function getPhonesDetails() {
+  try {
+    getPhones.then((data) => {
+      data.forEach(x => {
+        const htmlSegment
+        = `<div class="phoneName">
+          <p>${x.name}</p>
+        </div>`;
+
+        html += htmlSegment;
+      });
+
+      const container = document.querySelector('.container');
+
+      container.innerHTML = html;
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+getPhonesDetails();
