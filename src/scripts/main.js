aedf4492bc69ce7b3881434403c5b90c54be2f92
Katya Suvorova <katerinasuvorova97@gmail.com>
2022-11-08 17:49:43 +0200

    Fetch data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-// write your code here
+const BASE_URL = 'https://mate-academy.github.io/phone-catalogue-static/api';
+const list = document.createElement('ul');
+const body = document.querySelector('body');
+
+body.append(list);
+
+function requestData(endPoint) {
+  return fetch(`${BASE_URL}${endPoint}.json`)
+    .then(response => {
+      if (!response.ok) {
+        setTimeout(() => {
+          return new Error('Error');
+        }, 5000);
+      };
+
+      return response.json();
+    });
+};
+
+const getPhones = () => {
+  return requestData('/phones');
+};
+
+const getPhonesDetails = (id) => {
+  return requestData(`/phones/${id}`);
+};
+
+getPhones()
+  .then(data => {
+    return Promise.all(data.map(el => getPhonesDetails(el.id)));
+  })
+  .then(phones => {
+    phones.forEach(phone => {
+      list.insertAdjacentHTML('beforeend', `<li>${phone.name}</li>`);
+    });
+  });
