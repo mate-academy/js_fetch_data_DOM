e36623289ece5c4339be6f8b18d1c8508f614ebd
Demut <uddarendeli1@gmail.com>
2022-12-05 04:43:11 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,78 @@
 'use strict';
 
-// write your code here
+const request = (url) => {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error(`${response.status} - ${response.statusText}`);
+      }
+
+      if (!response.headers.get('content-type').includes('application/json')) {
+        throw Error('Content-type is not supported');
+      }
+
+      return response.json();
+    });
+};
+
+const storeData = (phone, details, result, container) => {
+  const element = document.createElement('p');
+  const desc = document.createElement('span');
+
+  result.push({
+    ...phone,
+    ...details,
+  });
+
+  element.innerHTML = result.length + '.) '
+    + result[result.length - 1].name;
+  desc.innerHTML = JSON.stringify(result[result.length - 1]);
+  container.appendChild(element);
+  container.append(desc);
+};
+
+// Using request
+const listURL = 'https://mate-academy.github.io/'
+  + 'phone-catalogue-static/api/phones.json';
+const detailsURL = 'https://mate-academy.github.io/'
+  + 'phone-catalogue-static/api/phones/';
+
+const getPhones = () => {
+  const BASE_URL = listURL;
+
+  return request(BASE_URL);
+};
+
+const getPhonesDetails = (phones) => {
+  const BASE_URL = detailsURL;
+  const container = document.createElement('div');
+  const result = [];
+
+  container.classList.add('container');
+
+  for (const phone of phones) {
+    request(BASE_URL + phone.id + '.json')
+      .then(data => {
+        storeData(phone, data, result, container);
+      });
+  }
+  document.body.appendChild(container);
+
+  return result;
+};
+
+const startTime = Date.now();
+
+const resolve = () => {
+  getPhones()
+    .then(getPhonesDetails)
+    .catch(err => {
+      if (startTime + 5000 < Date.now()) {
+        resolve();
+      }
+
+      return Promise.reject(err);
+    });
+};
+
+window.addEventListener('load', resolve);
